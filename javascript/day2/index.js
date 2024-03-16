9e8e7ac2f044071dd7c4721fe819e86cc42acec3
Suraj Kumar <surajsinhaking77@gmail.com>
2024-03-16 22:18:26 +0530

    day 2 javascript

---
+++ javascript/day2/index.js
@@ -0,0 +1,49 @@
+// javascript day 2 
+
+/*
+
+let c = 4; // global scope
+let b = 7;
+{
+    console.log("inside1 : "+ c);
+    //let c = 10  // local scope
+    console.log("inside2 : "+ c);
+}
+
+console.log("outside : "+c);  // outside :4 
+
+*/
+
+
+// for(let i =1; i<10; i++){ // loops
+    
+// };
+
+// console.log("print i here :", i);
+
+// for(let i =2; i<10; i++){
+    
+// }
+
+
+// Strings
+// samreddy harsha
+// 10:00 PM
+// let var1 ="Harsha"
+// let var2 ="vardhan"
+// console.log(var1+var2)
+
+
+// console.log("AB");
+
+// Monica Veluru 10:04 PM case sensitive
+// Vara ="Monica";
+// Varb ="Veluru";
+// console.log(vara+varb);
+
+// let a = 10;
+// let A = 1
+
+// console.log(a+A);
+
+
--- javascript/index.js
+++
@@ -1,5 +0,0 @@
-var _number1 = 13;
-var _number2 = 1;
-
-var result = _number1+_number2;
-console.log(result);
