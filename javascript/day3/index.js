3d1bca387653e575089304284a1b8278014ae3bc
Suraj Kumar <surajsinhaking77@gmail.com>
2024-03-18 22:02:32 +0530

    if else condition

---
+++ javascript/day3/index.js
@@ -0,0 +1,30 @@
+// const contant 
+// let a = false;
+
+// // a = 18%2;
+
+// a =  4 != 8;
+
+// console.log(a);
+
+
+// logical 
+
+// let ans = (4 != 4 || 9<=9);
+
+// console.log(ans);
+
+// which variable is greater 
+
+let a = 18;
+let b = 10;
+
+if(a > b){
+    if(a == b){
+        console.log("a is equal value ");
+    }else{
+        console.log("a is greater value ")
+    }
+}else{
+    console.log("b is greater value ");
+}
