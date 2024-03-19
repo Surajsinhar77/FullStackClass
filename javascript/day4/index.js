e063e6febedbee5c2af10a944d191927b2ee8637
Suraj Kumar <surajsinhaking77@gmail.com>
2024-03-19 22:15:05 +0530

    almost there

---
+++ javascript/day4/index.js
@@ -0,0 +1,21 @@
+
+let a = 8
+let b = 9
+// if(a > b){
+//     if(a == b){
+//         console.log("a is equal value ");
+//     }else{
+//         console.log("a is greater value ")
+//     }
+// }else{
+//     console.log("b is greater value ");
+// }
+
+
+if(a > b ){
+    console.log("a is greater");
+}else if(a == b){
+    console.log("a is equal to b");
+}else{
+    console.log("b is greater");
+}
---
+++ javascript/day4/loops.js
@@ -0,0 +1,39 @@
+// console.log("yours name ")
+// console.log("yours name ")
+// console.log("yours name ")
+// console.log("yours name ")
+// console.log("yours name ")
+
+
+// 0  to 10000000 
+// 2  
+
+// for(let i = 1 ; i<=10; i=i+2){   // i = 1 , 3, 5 , 7, 9 , 11
+//     console.log(" value  ", i )
+// }
+
+// 1. akhila joshi
+// let value = 6;
+// for(let j=1; j<=10; j++){ // j = 1, 2...,  11
+//     console.log(j*value)  //  6,  12.....60
+// }
+
+
+// for (let i=2; i<=10; i=i+2){
+//     for (let j=1; j<=10; j++){
+//         console.log("value is : "+i);
+//     }
+// }
+
+// 2
+// 4
+// 6
+// 8
+// 10
+// const btn = document.getElementById('btn');
+
+
+// btn.addEventListener('click', ()=>{
+//     const inputValue = document.getElementById("inputValue").value;
+//     console.log(inputValue)
+// })
