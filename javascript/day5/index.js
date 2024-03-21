694e92878f0fcd5a66542248d9e2a2c30d45b909
Suraj Kumar <surajsinhaking77@gmail.com>
2024-03-21 21:58:03 +0530

    doen almost some work

---
+++ javascript/day5/index.js
@@ -0,0 +1,65 @@
+// while
+
+// let i = 0
+// while(i<5){
+//     console.log("name"); //name name name name name
+//     i++; // 1 , 2, 3,  4, 5
+// }
+
+//i = 5 
+
+
+// for(let i =0; true; i++){
+
+// }
+// let value = true;
+// do{
+//     console.log("1 addition")
+//     console.log("2 div")
+//     console.log("3 multi  ")
+//     console.log("4 sub ")
+//     console.log("5 is for exit")
+//     let option = 4;
+//     if(option === 5){
+//         value = false
+//     }
+// }while(value)
+
+
+// ****
+// ****
+// ****
+// ****
+// ****
+// let  i = 0;
+// while(i<5){
+//     console.log("****");
+// }
+
+// let  i = 0;
+// while(i<5){
+//     let j =0
+//     let a = "*";
+//     let result = "";
+//     while(j<5){
+//         result = result+a;
+//         j++
+//     }
+//     console.log(result);
+//     i++
+// }
+
+
+// Homework
+
+// *
+// **
+// ***
+// ****
+// *****
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
