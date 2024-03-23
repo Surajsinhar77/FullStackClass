cdfbffbae0affdc51030281a1b994ba7078597f2
Suraj Kumar <surajsinhaking77@gmail.com>
2024-03-23 22:03:10 +0530

    doen almost some work

---
+++ javascript/day7/index.js
@@ -0,0 +1,76 @@
+
+
+// // // 1 add ,2 mult,3  div, 4 sub anyotherkey for exit 
+// let userValue = 0;
+
+// function add(x , y){
+//     console.log(x+y);
+// }
+
+// function mult(x , y){
+//     console.log(x*y);
+// }
+
+// function div(x , y){
+//     console.log(x/y);
+// }
+
+// function sub(x , y){
+//     console.log(x-y);
+// }
+// let a = 8;
+// let b = 2;
+
+// do{
+//     console.log("1 for add ");
+//     console.log("2 for mult ");
+//     console.log("3 for div ");
+//     console.log("4 for sub ");
+//     console.log("anyother key  for exit ");
+
+//     userValue = 1;
+
+//     if(userValue === 1){
+//         add(a, b);
+
+//     }else if(userValue === 2){
+//         mult(a,b);
+//     }else if(userValue === 3){
+//         div(a,b);
+//     }else if(userValue === 4){
+//         sub(a, b);
+//     }else{
+//         userValue = 0;
+//         // break;
+//     }
+
+//     userValue = 0;
+// }while(0);
+
+
+
+// function add(x , y){ //  let r;
+//     console.log(x+y);
+//     return x+y;
+// }
+
+// let returnValue = add(8, 2);
+// console.log("return value : ", returnValue);
+
+// let value ;
+
+// console.log(value);
+
+// value = 8;
+
+// console.log(value);
+
+// Array 
+
+let array = [1, 4, 8 , 9, 8, 0.6];
+
+let result = 0;
+for(let i =0; i<array.length; i++){
+    result = result+array[i];
+}
+console.log(result);
