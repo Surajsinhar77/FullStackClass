35d28ef27e12045f2eb7c4168bbbd55457f3420a
suraj Kumar <surajkumar71799@gmail.com>
2024-03-30 22:24:49 +0530

    done

---
+++ javascript/day10/index.js
@@ -0,0 +1,45 @@
+// "https://dummyjson.com/products/1"
+
+// let data = null;
+
+//  fetch("https://dummyjson.com/products")
+//     .then((res)=>{
+//         res.json().then((result) =>{
+//             console.log(result)
+//     }).catch((err)=>{
+//             console.log(err);
+//     })
+//  })
+
+
+ // object 
+
+
+
+const obj = {
+    nam : "Suraj",
+    class : "77",
+    uid : "asdasda",
+    rollNo : 1231,
+}
+
+
+obj.nam = "kk";
+
+console.log(obj.nam);
+
+// let section = "B"
+obj["section"] = "A";
+console.log(obj);
+
+
+
+
+
+
+// console.log(obj.nam, obj.class, obj.uid,  obj.rollNo);
+
+// let aray = []
+
+// console.log(typeof(aray));
+
