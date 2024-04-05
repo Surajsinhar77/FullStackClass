5f11841987c03f22eacfad8f8f5dc50e48c86d18
suraj Kumar <surajkumar71799@gmail.com>
2024-04-05 23:27:04 +0530

    done

--- miniProject/index.js
+++ miniProject/index.js
@@ -1,16 +1,52 @@
 let addCard = document.getElementById('addCard');
-let  array = []  
+let array = [];
 
     if(localStorage.getItem('cardData')){
         let a = JSON.parse(localStorage.getItem('cardData'));
-        console.log(a);
-        array.push(a);
-        console.log("localStorage ", array);
-    }else{
-        array = []
-    } 
+        for(let i =0; i<a.length; i++){
+            array.push(a[i]);
+        }
+    }
 
-console.log(array);
+        function allList(data, index){
+            console.log("data from all List", data);
+            const cardpart = document.getElementById("cardpart");
+
+            let cardForNote = `
+                <div class="card">
+                    <div class="content">
+                        <div>
+                            <label for="forTitle">Title</label>
+                            <input class="titleData" type="text">
+
+                        </div>
+                        <div class="textmsg">
+                            <label for="forText">Text</label>
+                            <textarea class="textData textmessage"> </textarea>
+                        </div>
+                    
+                    </div>
+                    <div class="btnArea">
+                        <button class="deleteBtn">Delete</button>
+                        <button class="saveBtn">Save</button>
+                        <button class="updateBtn">Update</button>
+                    </div>
+                </div>
+            `;
+
+            cardpart.insertAdjacentHTML('afterbegin', cardForNote);
+
+            let titleData = document.querySelector('.titleData');
+            let textData = document.querySelector('.textData');
+
+            titleData.value = data.titleName;
+            textData.value = data.textData;
+        }
+
+
+        array.forEach((element, index)=>{
+            allList(element, index);
+        })
 
 
         function addTheCard() {
@@ -54,12 +90,14 @@ console.log(array);
             let card = this.closest('.card');
             let titleData = card.querySelector('.titleData').value;
             let textData = card.querySelector('.textData').value;
+
             let obj = {
                 "titleName": titleData,
                 "textData": textData
             };
 
             array.push(obj);
+
             localStorage.setItem("cardData", JSON.stringify(array));
              
         }
